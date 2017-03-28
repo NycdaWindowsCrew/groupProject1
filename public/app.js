8d44626e5802a47a5173e9374dce5a3542e96911
ninjageek801 <richard.m.heflin@gmail.com>
2017-03-27 22:33:08 -0600

    background img and better forms, and welcome button

---
+++ public/app.js
@@ -0,0 +1,10 @@
+$("#loginbutton").on("click",function(){
+  $("#popup").show(),$("#loginbutton").hide();
+});
+
+$("#close1").on("click",function(){
+  $("#popup").hide(),$("#loginbutton").show();
+});
+$("#close2").on("click",function(){
+  $("#popup").hide(),$("#loginbutton").show();
+});
