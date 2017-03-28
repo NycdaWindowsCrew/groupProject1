270487586c417c83bd9ca568804c0ebe91c2d6b8
Camille Elmer <Camille Elmer>
2017-03-27 19:39:41 -0600

    test form to input information

---
+++ public/app.js
@@ -0,0 +1,15 @@
+//when 'x.png is hovered over the image is made larger'
+
+function animate(){
+        document.getElementById('x.png').style.webkitTransitionDuration='1s';
+        document.getElementById('x.png').style.backgroundSize="200% 200%";
+	}
+// </script>
+// <div id="a" onmouseover="animate()" style="width: 200px; height: 70px; border: 1px solid; background: url('http://www.wpclipart.com/food/fruit/apple/apples_4/apple_honeycrisp_small.png') no-repeat;background-size: 100% 100%; ">
+// </div>
+
+function enlarge (){
+	document.getElementById("x.png").addEventListener("hover");
+	zoom: 0.5;
+	-moz-transform: scale(.5);
+};
